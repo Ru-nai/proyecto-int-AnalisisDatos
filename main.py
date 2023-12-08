ff363ce6f4e966280d0344ed3f321f8f306f4a6b
Laura <lllauracb@gmail.com>
2023-12-08 15:03:39 -0500

    proporción de mujeres fumadoras por cada hombre fumador

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ print(f"Promedio de edad de los participantes: {promedio_edades} años")
 separador()
 
 print(' --- PROYECTO INTEGRADOR PARTE 2 --- ')
+
+#1. Convertir la estructura Dataset en un DataFrame de Pandas usando pd.DataFrame.
 df = pd.DataFrame(data)
 print(df)
 
+#2. Separar el dataframe en dos diferentes, uno conteniendo las filas con personas que perecieron (is_dead=1) y otro con el complemento.
 df_is_dead = df[df['is_dead'] == 1]
 df_isnot_dead = df[df['is_dead'] == 0]
 
@@ -29,6 +32,7 @@ print(f"Dataframe de las personas que fallecieron: \n {df_is_dead}")
 separador()
 print(f"Dataframe de las personas que NO fallecieron: \n {df_isnot_dead}")
 
+#3. Calcular los promedios de las edades de cada dataset e imprimir.
 promedio_edad_fallecido = round(np.mean(df_is_dead['age']), 2)
 promedio_edad_nofallecido = round(np.mean(df_isnot_dead['age']), 2)
 
@@ -39,6 +43,7 @@ print(f"Promedio de edad de los que NO fallecieron: {promedio_edad_nofallecido} 
 separador()
 
 print(' --- PROYECTO INTEGRADOR PARTE 3 --- ')
+#1. Verificar que los tipos de datos son correctos en cada colúmna (por ejemplo que no existan colúmnas numéricas en formato de cadena).
 tipo_de_datos = df.dtypes
 print(f"El tipo de dato de cada columna es: \n", tipo_de_datos)
 separador()
@@ -51,10 +56,15 @@ separador()
 print(f"El nuevo dataframe se ve así: \n {df}")
 separador()
 
+#2. Calcular la cantidad de hombres fumadores vs mujeres fumadoras (usando agregaciones en Pandas).
 cuenta_hombre_fumador = df[(df['is_smoker'] == True) & (df['is_male'] == True)].shape[0]
 cuenta_mujer_fumador = df[(df['is_smoker'] == True) & (df['is_male'] == False)].shape[0]
+proporcion_mujer_x_hombre = cuenta_mujer_fumador / cuenta_hombre_fumador if cuenta_hombre_fumador != 0 else 0
 print(f"La cantidad de hombres fumadores es: {cuenta_hombre_fumador}")
 print(f"La cantidad de mujeres fumadoras es: {cuenta_mujer_fumador}")
+print(f"La proporción de fumadoras por cada fumador es: {proporcion_mujer_x_hombre:.2f}")
+
+
 
 
     
