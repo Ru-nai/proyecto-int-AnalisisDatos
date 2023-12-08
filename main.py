e65ff7d5bd109ac47136504f5abcfa6bdcdff59c
Laura <lllauracb@gmail.com>
2023-12-08 15:08:35 -0500

    analisis_datos_4

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ separador()
 separador()
 
 print(' --- PROYECTO INTEGRADOR PARTE 2 --- ')
+
+#1. Convertir la estructura Dataset en un DataFrame de Pandas usando pd.DataFrame.
 df = pd.DataFrame(data)
 print(f"El Dataframe para este proyecto es: \n {df}")
 
+#2. Separar el dataframe en dos diferentes, uno conteniendo las filas con personas que perecieron (is_dead=1) y otro con el complemento.
 df_is_dead = df[df['is_dead'] == 1]
 df_isnot_dead = df[df['is_dead'] == 0]
 separador()
@@ -34,6 +37,7 @@ print(f"Dataframe de las personas que NO fallecieron: \n {df_isnot_dead}")
 separador()
 separador()
 
+#3. Calcular los promedios de las edades de cada dataset e imprimir.
 promedio_edad_fallecido = round(np.mean(df_is_dead['age']), 2)
 promedio_edad_nofallecido = round(np.mean(df_isnot_dead['age']), 2)
 print(f"Promedio de edad de los fallecidos: {promedio_edad_fallecido} años")
@@ -42,6 +46,7 @@ separador()
 separador()
 
 print(' --- PROYECTO INTEGRADOR PARTE 3 --- ')
+#1. Verificar que los tipos de datos son correctos en cada colúmna (por ejemplo que no existan colúmnas numéricas en formato de cadena).
 tipo_de_datos = df.dtypes
 print(f"El tipo de dato de cada columna es: \n", tipo_de_datos)
 separador()
@@ -57,6 +62,7 @@ print(f"El nuevo dataframe se ve así: \n {df}")
 separador()
 separador()
 
+#2. Calcular la cantidad de hombres fumadores vs mujeres fumadoras (usando agregaciones en Pandas).
 cuenta_hombre_fumador = df[(df['is_smoker'] == True) & (df['is_male'] == True)].shape[0]
 cuenta_mujer_fumador = df[(df['is_smoker'] == True) & (df['is_male'] == False)].shape[0]
 print(f"La cantidad de hombres fumadores es: {cuenta_hombre_fumador}")
@@ -65,7 +71,8 @@ separador()
 separador()
 
 print(' --- PROYECTO INTEGRADOR PARTE 4 --- ')
-
+#1. Realiza un GET request para descargarlos y escribe la respuesta como un archivo de texto plano con extensión csv (no necesitas pandas para esto, sólo manipulación de archivos nativa de Python)
+#2. Agrupa el código para esto en una función reutilizable que reciba como argumento la url.
 def descarga_get_request (url, salida_archivo):
     response = requests.get(url)
 
