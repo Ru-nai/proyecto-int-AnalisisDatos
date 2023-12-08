68382041bd6c4937a9a46ab3996823977d577cc3
Laura <lllauracb@gmail.com>
2023-12-08 14:57:50 -0500

    analisis_datos_2

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ print(f"Promedio de edad de los participantes: {promedio_edades} años")
 
 """ --- PROYECTO INTEGRADOR PARTE 2 --- """
 
+#1. Convertir la estructura Dataset en un DataFrame de Pandas usando pd.DataFrame.
 df = pd.DataFrame(data)
 print(df)
 
+#2. Separar el dataframe en dos diferentes, uno conteniendo las filas con personas que perecieron (is_dead=1) y otro con el complemento.
 df_is_dead = df[df['is_dead'] == 1]
 df_isnot_dead = df[df['is_dead'] == 0]
 
 print(f"Dataframe de las personas que fallecieron: {df_is_dead}")
 print(f"Dataframe de las personas que NO fallecieron: {df_isnot_dead}")
 
+#3. Calcular los promedios de las edades de cada dataset e imprimir.
 promedio_edad_fallecido = round(np.mean(df_is_dead['age']))
 promedio_edad_nofallecido = round(np.mean(df_isnot_dead['age']))
 
